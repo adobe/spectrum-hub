ee30c36416fd1845885a7411081bfc11e0518e25
marissahuysentruyt <marissa.huysentruyt@heysparkbox.com>
2026-06-12 15:05:25 -0400

    feat(ue-sidenav): add uec sidenav component and build step

---
+++ deps/uec-sidenav/src/index.js
@@ -0,0 +1,8 @@
+import '@uec-labs/sidenav/ue-sidenav.js';
+import '@uec-labs/sidenav/ue-sidenav-item.js';
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/spectrum-two/theme-light.js';
+import '@spectrum-web-components/theme/spectrum-two/theme-dark.js';
+import '@spectrum-web-components/theme/spectrum-two/scale-medium.js';
+import '@spectrum-web-components/overlay/sp-overlay.js';
+import '@spectrum-web-components/underlay/sp-underlay.js';
