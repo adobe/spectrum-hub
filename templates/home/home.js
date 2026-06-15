fdab8ed45ac4ae7abb58b0d2562bcbfd8e39b50c
marissahuysentruyt <marissa.huysentruyt@heysparkbox.com>
2026-06-15 08:10:35 -0400

    feat(home): add global sidenav to template grid

--- templates/home/home.js
+++ templates/home/home.js
@@ -1,8 +1,15 @@
-export default function init() {
-  const heading = document.querySelector('h1');
-  heading.classList.add('heading-size-xxxxl');
+export default async function init() {
+  const main = document.querySelector('main');
 
+  const heading = main.querySelector('h1');
+  heading.classList.add('heading-size-xxxxl');
   const parent = heading.closest('div');
   parent.className = 'home-column';
   parent.nextElementSibling.append(parent);
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'template-wrapper';
+
+  main.replaceWith(wrapper);
+  wrapper.append(main);
 }
