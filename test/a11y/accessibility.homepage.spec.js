00e028c1dfefeef2a237b216ed38bbd09132aaea
marissahuysentruyt <marissa.huysentruyt@heysparkbox.com>
2026-08-17 10:57:35 -0400

    fix(a11y): homepage waits for network to load all elements before tests

--- test/a11y/accessibility.homepage.spec.js
+++ test/a11y/accessibility.homepage.spec.js
@@ -4,6 +4,7 @@ import { test, expect } from './axe-test.js';
 test.describe('homepage', () => {
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('/');
+    await page.waitForLoadState('networkidle');
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
@@ -12,6 +13,7 @@ test.describe('homepage', () => {
 
   test('should not have any automatically detectable WCAG 2.2 A or AA violations', async ({ page, makeAxeBuilder }) => {
     await page.goto('/');
+    await page.waitForLoadState('networkidle');
 
     const accessibilityScanResults = await makeAxeBuilder().analyze();
 
