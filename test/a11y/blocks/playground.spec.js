fb223157252a572a946a41bf96fe672ad1c001b2
marissahuysentruyt <marissa.huysentruyt@heysparkbox.com>
2026-09-21 13:52:57 -0400

    fix(a11y): fix snapshot for swc-button

--- test/a11y/blocks/playground.spec.js
+++ test/a11y/blocks/playground.spec.js
@@ -63,7 +63,7 @@ test(`${block.name} block matches its expected accessibility tree`, async ({ pag
     - text: isDisabled
     - button "Copy code"
     - status
-    - text: <swc-button> Button </swc-button>
+    - text: <swc-button size="m"> Get started </swc-button>
   `);
 });
 
